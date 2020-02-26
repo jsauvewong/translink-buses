bfc976b2837f93ab053c8bdf5a3e6c048b7fe884
JS <jsauvewong@gmail.com>
2020-02-25 21:48:21 -0800

    centered bus <3

--- src/Marker.js
+++ src/Marker.js
@@ -1,6 +1,6 @@
 import React, {PureComponent} from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
-import logo from './logo.svg';
+import logo from './school-bus.svg';
 
 const Vancouver = {name:"Vancouver", longitude:-123.107176 ,latitude:49.282842}
 const CITIES = [Vancouver];
@@ -9,7 +9,13 @@ const CITIES = [Vancouver];
 export class Markers extends PureComponent {
   render() {
     return CITIES.map(
-      city => <Marker key={city.name} longitude={city.longitude} latitude={city.latitude} ><img src={logo}className="App-logo" alt="logo"  /></Marker>
+      city => <Marker key={city.name} 
+                longitude={city.longitude} 
+                latitude={city.latitude} 
+                offsetLeft={-25} 
+                offsetTop={-25} >
+          <img src={logo} style={{height:50}} alt="logo"  />
+      </Marker>
     )
   }
 }
